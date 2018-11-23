8c5deae1f1d242630e5f7483d0d033e8a60f7d4d
Mrxx99 <33566379+Mrxx99@users.noreply.github.com>
2018-11-23 17:28:35 +0100

    Update Updater.cmd

--- Updater.cmd
+++ Updater.cmd
@@ -6,15 +6,19 @@ git pull upstream master
 git checkout upstream/master
 
 cd ..\dependencies
-for /f %%i in ('git pull origin master') do set codeOut=%%i
+git pull origin master
 git checkout origin/master
-cd..
+cd ..
 xcopy /s /y /i "dependencies\windows_64bit\dependencies" "stk-code\dependencies-64bit"
 cd stk-code\build
 cmake.exe ..	
 cd ..\..
 
 REM TODO: check if dependencies are uptodate and only copy and cmake if not
+cd ..\dependencies
+for /f %%i in ('git pull origin master') do set codeOut=%%i
+git checkout origin/master
+cd..
 REM IF NOT "codeOut"=="Already" (
 REM	xcopy /s /y /i "dependencies\windows_64bit\dependencies" "stk-code\dependencies-64bit"
 REM	cd stk-code\build
