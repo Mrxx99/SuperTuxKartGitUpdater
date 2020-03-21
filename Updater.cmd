2663616a8597e2bb56ab066d9149c22b9e56e8c3
Mrxx99 <33566379+Mrxx99@users.noreply.github.com>
2020-03-21 18:04:23 +0100

    updated build script
    always running cmake and rebuilding solution

--- Updater.cmd
+++ Updater.cmd
@@ -2,29 +2,18 @@ cd stk-assets
 svn update
 
 cd ..\stk-code
+git checkout master
 git pull upstream master
-git checkout upstream/master
+
 
 cd ..\dependencies
+git checkout master
 git pull origin master
-git checkout origin/master
-cd ..
+cd..
+
 xcopy /s /y /i "dependencies\windows_64bit\dependencies" "stk-code\dependencies-64bit"
 cd stk-code\build
-cmake.exe ..	
-cd ..
+cmake.exe ..
 
-REM TODO: check if dependencies are uptodate and only copy and cmake if not
-cd ..\dependencies
-for /f %%i in ('git pull origin master') do set codeOut=%%i
-git checkout origin/master
-cd..
-REM IF NOT "codeOut"=="Already" (
-REM	xcopy /s /y /i "dependencies\windows_64bit\dependencies" "stk-code\dependencies-64bit"
-REM	cd stk-code\build
-REM	cmake.exe ..	
-REM	cd ..\..
-REM )
-
-msbuild.exe stk-code\build\SuperTuxKart.sln /t:Build /p:Configuration=Release
+msbuild.exe SuperTuxKart.sln /t:Rebuild /p:Configuration=Release
 pause
